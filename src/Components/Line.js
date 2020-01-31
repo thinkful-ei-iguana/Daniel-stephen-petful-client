298933376ae3cdd03dc395854bdc790978f5fb0e
Daniel Kent <Kentpercussion@gmail.com>
2020-01-31 15:26:44 -0700

    added

--- src/Components/Line.js
+++ src/Components/Line.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 
 class Line extends Component {
   render() {
-    const users = this.props;
+    const { users } = this.props;
 
     return (
-      <ul className="Line">
-        {users.map(user => {
-          return <li className="Line__user-name">{user.user_name}</li>
-        })}
-      </ul>
+      <div className="user-line-container">
+        <h3>Users waiting to adopt</h3>
+        <ul className="Line">
+          {users.map((user, i) => {
+            return <li className="Line__user-name" key={i}>{user.user_name}</li>
+          })}
+        </ul>
+      </div>
     )
   }
 }
