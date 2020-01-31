de6276e11d2390cb634965bdbdd553f924d45c69
stephenschab <sschab689@gmail.com>
2020-01-31 15:02:30 -0700

    added styles to landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './LandingPage.css';
 
 function LandingPage() {
   return (
     <div className="LandingPage">
-      <h1 className="LandingPage__logo">DS-Petful</h1>
-
       <img src={require("../images/petful-landing-page.jpg")} alt="Woman holding her newly adopted dog" className="LandingPage__img"/>
 
-      <p className="LandingPage__content">Welcome to DS-Petful. We are a pet adoption service with a twist! Only one pet is up for adoption at a time. You can join the adoption line any time you please, but can only adopt a pet once you are at the FRONT of the line. You are able to see where your position in line is but can only see the cat and dog that are up next.</p>
+      <h1 className="LandingPage__logo">DS-Petful</h1>
 
-      <button className="LandingPage__button"><Link>Enter</Link></button>
+      <div className="LandingPage__content">
+        <p>Welcome to DS-Petful. We are a pet adoption service with a twist! Only one pet is up for adoption at a time. You can join the adoption line any time you please, but can only adopt a pet once you are at the FRONT of the line. You will be able to see where your position in line is but can only see the cat and dog that are up next.</p>
+      </div>
+      
+
+      <button className="LandingPage__button"><Link to={'/adoptions'} className="LandingPage__link">Enter</Link></button>
     </div>
   );
 }
